52f0f9e7e6aec1d6ae1eed1eaccbc2b2ff402977
Matvey <sinema3d00@gmail.com>
2024-08-14 20:41:58 +0300

    changing homecontroller to get name of admin

--- ASPLesson/Controllers/HomeController.cs
+++ ASPLesson/Controllers/HomeController.cs
@@ -7,14 +7,17 @@ namespace ASPLesson.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var adminName = _configuration.GetSection("Admin:Name");
             return View();
         }
 
